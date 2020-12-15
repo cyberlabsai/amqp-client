7445827103214737edad970780d1a52c6cd452e6
code4trade <luccazanchitrader@gmail.com>
2020-12-15 13:48:11 -0300

    Rename worker struct to event handler

--- worker/worker.go
+++ worker/worker.go
@@ -7,27 +7,27 @@ import (
 	"github.com/streadway/amqp"
 )
 
-// Events is a type that contains a list of events and their function.
-// The key is the name and the value is what the worker will do when consume a message with this event.
+// Topics is a type that contains a list of topics and their function.
+// The key is the name and the value is what the handler will do when consume a message with this topic.
 // Ex:
-//   event: 	"deleted"
+//   topic: 	"deleted"
 //	 function: 	onDelete(msg amqp.Delivery) bool { // delete from storage }
-type Events map[string]func(amqp.Delivery) bool
+type Topics map[string]func(amqp.Delivery) bool
 
-// Worker consume messages and process according their instructions.
-// The service help the worker to connect and bind a topic to receive the messages from network.
-// The Events can be added in safe mode with the function AddEvent, or directly.
-type Worker struct {
+// EventHandler consume messages and process according their instructions.
+// The service help the EventHandler to connect and bind a topic to receive the messages from network.
+// The Topics can be added in safe mode with the function AddTopic, or directly.
+type EventHandler struct {
 	*client.Service
-	Events
+	Topics
 }
 
-// New returns a created and connected worker.
-// The worker has a service that will declare a new queue
+// New returns a created and connected EventHandler.
+// The EventHandler has a service that will declare a new queue
 // and link to a topic.
 // Before consuming the messages it's necessary to add at least one event
-// on your map, after that, the worker can be started successfully.
-func New(topicPrefix, address, exchange, contentType, queueName string) (*Worker, error) {
+// on your map, after that, the EventHandler can be started successfully.
+func New(topicPrefix, address, exchange, contentType, queueName string) (*EventHandler, error) {
 
 	s := client.New(topicPrefix, address, exchange, contentType)
 	if err := s.Start(); err != nil {
@@ -46,29 +46,26 @@ func New(topicPrefix, address, exchange, contentType, queueName string) (*Worker
 		return nil, err
 	}
 
-	return &Worker{
+	return &EventHandler{
 		Service: s,
 	}, nil
 }
 
-// AddEvent receive a event name and the instruction to process the message of this event.
-func (w *Worker) AddEvent(event string, do func(amqp.Delivery) bool) error {
-	if _, ok := w.Events[event]; ok {
-		return fmt.Errorf("The event %q already exists", event)
+// Start initiates the EventHandler to consuming and process the messages,
+// the handle parameter is a type of function that receive instructions to prepare de variables to be used on topic functions,
+// this is necessary because a message need pre processing to handle each event.
+func (w *EventHandler) Start(handle func(topics Topics, msg amqp.Delivery), messages <-chan amqp.Delivery) {
+	for msg := range messages {
+		handle(w.Topics, msg)
 	}
-
-	w.Events[event] = do
-	return nil
 }
 
-// Work is a type of function that receive instructions to process events.
-// This is necessary because somethimes the worker need pre processing in different ways.
-type Work func(events Events, msg amqp.Delivery)
-
-// Start initiates the worker to consuming and process the messages.
-func (w *Worker) Start(do Work, messages <-chan amqp.Delivery) {
-	for msg := range messages {
-		// Don't need to wait the process of one message to process another. This is good?
-		go do(w.Events, msg)
+// AddTopic receive a topic name and the instruction to process the message received for this topic.
+func (w *EventHandler) AddTopic(topic string, handleMsgFn func(amqp.Delivery) bool) error {
+	if _, ok := w.Topics[topic]; ok {
+		return fmt.Errorf("The event %q already exists", topic)
 	}
+
+	w.Topics[topic] = handleMsgFn
+	return nil
 }
